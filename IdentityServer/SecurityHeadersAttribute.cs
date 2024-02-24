b62b9c087a3b17a27e725d01e117ea2a0362eeb2
Stu Frankish <contact@stufrankish.dev>
2024-02-24 13:43:41 +0000

    Minor file movement.
    - Moved Test Users store.
    - Moved SecurityHeaders attribute.

--- IdentityServer/SecurityHeadersAttribute.cs
+++ IdentityServer/SecurityHeadersAttribute.cs
@@ -5,7 +5,7 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IdentityServerHost.Pages;
+namespace IdentityServerHost;
 
 public class SecurityHeadersAttribute : ActionFilterAttribute
 {
